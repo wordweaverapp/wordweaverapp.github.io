49b413beac910aca394b7406f7a15049eabd2845
draftsman-io <system@draftsman.io>
2023-11-26 15:32:26 +0000

    D1562559209:personal:WordWeaver

--- js/configuration-staging.js
+++ js/configuration-staging.js
@@ -1,5 +1,5 @@
-localStorage["aws-congnito-user-pool-id"] = "eu-north-1_9WGQyYUrL";
-localStorage["aws-congnito-app-id"] = "7lhc388telutvo43qu3jgf5ine";
-const api_url = "https://ekh4oi2gind7rj735lb3d36ewy.appsync-api.eu-north-1.amazonaws.com/graphql";
-const api_ws = "wss://ekh4oi2gind7rj735lb3d36ewy.appsync-realtime-api.eu-north-1.amazonaws.com/graphql";
-const api_key = "da2-y2uwpusazrhkjga5h3wsrjft4m";
+localStorage["aws-congnito-user-pool-id"] = "eu-north-1_1uuqrkur9";
+localStorage["aws-congnito-app-id"] = "7h8psleh2be2td5phdsh5qiuqv";
+const api_url = "https://mkl7ktoeyjhgnee7xx2csc6yxa.appsync-api.eu-north-1.amazonaws.com/graphql";
+const api_ws = "wss://mkl7ktoeyjhgnee7xx2csc6yxa.appsync-realtime-api.eu-north-1.amazonaws.com/graphql";
+const api_key = "da2-wopoykqrdnghbjhefm7ng742ay";
