48466bd13b1bc579950d69ce8b05a6ad154e0e7b
bohanssen <bohanssen@gmail.com>
2025-03-15 10:42:48 +0100

    Update configuration.js

--- js/configuration.js
+++ js/configuration.js
@@ -1,5 +1,6 @@
 localStorage["aws-congnito-user-pool-id"] = "eu-north-1_COXzNoToy";
 localStorage["aws-congnito-app-id"] = "7go1ct60uc1r33i0bofbtmpbp2";
+localStorage["aws-congnito-ui"] = "https://wordweaver-prd.auth.eu-north-1.amazoncognito.com";
 const api_url = "https://ne5ppb323faurfosgvmoxjs4e4.appsync-api.eu-north-1.amazonaws.com/graphql";
 const api_ws = "wss://ne5ppb323faurfosgvmoxjs4e4.appsync-realtime-api.eu-north-1.amazonaws.com/graphql";
 const api_key = "da2-77eq62jkdrcyfjvjtoj42mdhvi";
